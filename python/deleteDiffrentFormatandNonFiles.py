2a5537e83cf8003a95c3441c20fc20a8dd1ea74a
kentoak <kt.fly.giants@gmail.com>
2023-02-09 17:18:25 +0900

    m

--- python/deleteDiffrentFormatandNonFiles.py
+++ python/deleteDiffrentFormatandNonFiles.py
@@ -5,13 +5,14 @@ from pprint import pprint
 import os
 
 # limit を適宜変更のこと
-#urlget = "http://10.178.1.27:8888/api/recorded?isHalfWidth=false&offset=0&limit=10000"
-urlget = "http://100.118.14.25:8888/api/recorded?isHalfWidth=false&offset=0&limit=10000"
+urlget = "http://10.178.1.27:8888/api/recorded?isHalfWidth=false&offset=0&limit=10000"
+#urlget = "http://100.118.14.25:8888/api/recorded?isHalfWidth=false&offset=0&limit=10000"
 response = urllib.request.urlopen(urlget)
 jsonData = json.load(response)
 
-#dirPath="/mnt/hdd1/"
-dirPath="/Users/kt/mountpoint1/"
+dirPath="/mnt/hdd1/"
+#dirPath="/Users/kt/mountpoint1/"
+delteFlag=False
 
 def deleteFile(url):
     headers = {"Content-Type": "application/json"}
@@ -50,17 +51,17 @@ for jsonObj in reversed(jsonData["records"]):
             print("形式が違います。これ削除ね")
             #url="http://100.118.14.25:8888/api/videos/:"+videoFileObj["id"]
             url="http://10.178.1.27:8888/api/videos/"+str(videoFileObj["id"])
-            deleteFile(url)
+            if delteFlag:deleteFile(url)
         
         if type=="encoded" and file_extension==".ts": #filenameがtsなのにtypeが'encoded'になっているものは削除
             print("形式が違います。これ削除ね")
             #url="http://100.118.14.25:8888/api/videos/:"+videoFileObj["id"]
             url="http://10.178.1.27:8888/api/videos/"+str(videoFileObj["id"])
-            deleteFile(url)
+            if delteFlag:deleteFile(url)
             
         if type=="ts" and not os.path.exists(dirPath+filename): #recordedにtsファイルがないのにtsをもっているものは削除
             print("tsファイルはrecordedにありません。削除ね")
             #url="http://100.118.14.25:8888/api/videos/:"+videoFileObj["id"]
             url="http://10.178.1.27:8888/api/videos/"+str(videoFileObj["id"])
-            deleteFile(url)
+            if delteFlag:deleteFile(url)
         #sizeEncoded=videoFileObj["size"]
--- python/deleteFiles.py
+++ python/deleteFiles.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import time
+import sys
 
 # limit を適宜変更のこと
 #urlget = "http://10.178.1.27:8888/api/recorded?isHalfWidth=false&offset=0&limit=10000"
@@ -8,35 +9,46 @@ urlget = "http://10.178.1.27:8888/api/recorded?isHalfWidth=false&offset=0&limit=
 # 録画一覧を得る
 response = urllib.request.urlopen(urlget)
 jsonData = json.load(response)
-# 録画ごとにループ
-for jsonObj in reversed(jsonData["records"]):
-    # "videoFilesを取り出し
-    flagEncoded=False
-    flagTs=False
-    recordid=jsonObj["id"]
-    #print("recordid:",recordid)
-    if not jsonObj["videoFiles"]:
-        print("recordid:",recordid)
-        print("これファイルないね、消します")
-        #url="http://10.178.1.27:8888/api/videos/"+str(recordid)
-        url="http://10.178.1.27:8888/api/recorded/"+str(recordid)
-        #url="http://10.178.1.27:8888/api/recorded/detail/"+str(recordid)
-        headers = {"Content-Type": "application/json"}
-        res=urllib.request.Request(url,json.dumps(jsonObj).encode(),headers,method="DELETE")
-        try:
-            with urllib.request.urlopen(res) as f:
-                pass
-            print(f.status,end="")
-            if (200==f.status):
-                print(" OK",end="")
-            else:
-                print(" Error",end=" ")
-        except urllib.error.HTTPError as err:
-            print(" Error",end=" ")
-            print(err.code,end=" ")
-        except urllib.error.URLError as err:
-            print(" Error",end="")
-            print(err.reason)
-    # for videoFileObj in jsonObj["videoFiles"]:
-    #     print(videoFileObj["filename"])
-    #     print("id:",videoFileObj["id"],videoFileObj["type"])
+delteFlag=False
+input_data = sys.argv[1:]
+print("input is ", input_data)
+if input_data[0]=="1":
+    delteFlag=True
+
+def main():
+    # 録画ごとにループ
+    if delteFlag:
+        print("不要ファイルを本当に削除します...")
+        time.sleep(3)
+
+    for jsonObj in reversed(jsonData["records"]):
+        # "videoFilesを取り出し
+        recordid=jsonObj["id"]
+        #print("recordid:",recordid)
+        if not jsonObj["videoFiles"]:
+            print("recordid:",recordid,"filename:",jsonObj["name"])
+            print("これファイルないね、消します")
+            if delteFlag:
+                #url="http://10.178.1.27:8888/api/videos/"+str(recordid)
+                url="http://10.178.1.27:8888/api/recorded/"+str(recordid)
+                #url="http://10.178.1.27:8888/api/recorded/detail/"+str(recordid)
+                headers = {"Content-Type": "application/json"}
+                res=urllib.request.Request(url,json.dumps(jsonObj).encode(),headers,method="DELETE")
+                try:
+                    with urllib.request.urlopen(res) as f:
+                        pass
+                    print(f.status,end="")
+                    if (200==f.status):
+                        print(" OK",end="")
+                    else:
+                        print(" Error",end=" ")
+                except urllib.error.HTTPError as err:
+                    print(" Error",end=" ")
+                    print(err.code,end=" ")
+                except urllib.error.URLError as err:
+                    print(" Error",end="")
+                    print(err.reason)
+                print("\n")
+            
+if __name__ == "__main__":
+	main()
